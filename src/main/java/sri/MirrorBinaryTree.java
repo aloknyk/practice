6b75bd1f81c849c754b91881f10e5157dce005a2
Srinath-R <r.srinath@live.com>
2017-10-07 00:39:43 +0530

    Mirror binary tree.

---
+++ src/main/java/sri/MirrorBinaryTree.java
@@ -0,0 +1,61 @@
+package sri;
+
+public class MirrorBinaryTree {
+	private class Node {
+		int data;
+		Node left,right;
+		
+		Node(int item){
+			this.data = item;
+			left=null;
+			right=null;
+		}
+	}
+	
+	Node tree;
+	
+	private Node insertLeft(Node t,int item){
+		if(null != t.left){
+			t.left = insertLeft(t.left, item);
+		}
+		else {
+			t.left = new Node(item);
+		}
+		return t;
+	}
+	
+	private Node insertRight(Node t,int item){
+		if(null != t.right){
+			t.right = insertRight(t.right, item);
+		}
+		else {
+			t.right = new Node(item);
+		}
+		return t;
+	}
+
+	public static void main(String[] args) {
+		new MirrorBinaryTree().start();
+
+	}
+
+	private  void start() {
+		tree = new Node(10);
+		insertLeft(tree, 20);
+		insertLeft(tree, 40);
+		insertRight(tree, 30);
+		
+		tree = mirrorTree(tree);
+		System.out.println("Test");
+	}
+
+	private Node mirrorTree(Node t) {
+		if(null != t){
+			Node tmp = t.left;
+			t.left = mirrorTree(t.right);
+			t.right = mirrorTree(tmp);
+		}
+		return t;
+	}
+
+}
