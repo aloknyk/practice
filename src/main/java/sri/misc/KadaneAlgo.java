decb689ada17487c25ca423f2c41689426aa6baf
Srinath-R <r.srinath@live.com>
2018-08-10 18:49:56 +0530

    package name changes.

---
+++ src/main/java/sri/misc/KadaneAlgo.java
@@ -0,0 +1,24 @@
+package sri.misc;
+
+public class KadaneAlgo {
+	public static void main(String[] args) {
+		int[] arr = {-1,-2,-3,-4};
+		int kadaneSum = kSum(arr);
+		System.out.println(kadaneSum);
+	}
+
+	private static int kSum(int[] arr) {
+		int mi = arr[0];
+		//let array's max value be ksum
+		int ksum = arr[0];
+		for(int i=1;i<arr.length;i++){
+			mi = max(arr[i],mi+arr[i]);
+			ksum = max(mi,ksum);
+		}
+		return ksum;
+	}
+
+	private static int max(int x,int y) {
+		return (x>y) ? x : y;
+	}
+}
